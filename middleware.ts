f228c73fa2c10fa83df6a46917e54655b0a0741f
trosystems1 <trosystems@gmail.com>
2026-08-04 19:33:58 +0900

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,4 +1,5 @@
 export { default } from 'next-auth/middleware'
 export const config = {
-  matcher: ['/((?!api/auth|api/cron|api/settings|login|_next/static|_next/image|favicon.ico).*)'],
+matcher: ['/((?!api/auth|api/aria|api/cron|api/settings|login|_next/static|_next/image|favicon.ico).*)'],
 }
+
